c13c8a177bc21de646897123808cc6267fea2edd
Tony95Montana <100350821+Tony95Montana@users.noreply.github.com>
2024-04-25 14:03:16 +0200

    Update artiste.service.ts

--- src/artiste/artiste.service.ts
+++ src/artiste/artiste.service.ts
@@ -5,27 +5,24 @@ import { ArtisteEntity } from './artiste.entity/artiste.entity';
 
 @Injectable()
 export class ArtisteService {
-  constructor(
-    @InjectRepository(ArtisteEntity)
-    private ArtistesRepository: Repository<ArtisteEntity>,
-  ) {}
+
+  constructor(@InjectRepository(ArtisteEntity) private ArtistesRepository: Repository<ArtisteEntity>) { }
 
   async getArtistes(): Promise<ArtisteEntity[]> {
     return await this.ArtistesRepository.find({ relations: ['Musique'] });
   }
-
-  async getArtiste(_id: number): Promise<ArtisteEntity> {
-    return await this.ArtistesRepository.findOneBy({ id: _id });
+  async getArtiste(id: number): Promise<ArtisteEntity> {
+    return await this.ArtistesRepository.findOneBy({ id: id });
   }
-
+  async getArtisteByName(nom: string): Promise<ArtisteEntity> {
+    return await this.ArtistesRepository.findOneBy({ nom: nom });
+  }
   async createArtiste(Artiste: ArtisteEntity) {
     return await this.ArtistesRepository.save(Artiste);
   }
-
   async updateArtiste(Artiste: ArtisteEntity) {
     return await this.ArtistesRepository.update(Artiste.id, Artiste);
   }
-
   async deleteArtiste(Artiste: ArtisteEntity) {
     return await this.ArtistesRepository.delete(Artiste);
   }
