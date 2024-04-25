39b0eac973c0cc867bdaec6de3f114fb93ed7f4d
Tony95Montana <100350821+Tony95Montana@users.noreply.github.com>
2024-04-25 10:54:50 +0200

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UtilisateurService } from 'src/utilisateur/utilisateur.service';
 
@@ -6,28 +6,19 @@ const bcrypt = require('bcrypt');
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly utilisateursService: UtilisateurService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private readonly utilisateursService: UtilisateurService, private jwtService: JwtService) { }
 
-  async validateUser(username: string, pass: string) {
-    const user = await this.utilisateursService.findOne(username);
-    console.log(user);
+  async validateUser(email: string, motDePasse: string) {
+    const user = await this.utilisateursService.findOne(email);
     const { mdp, ...result } = user;
-    const isMatch = await bcrypt.compare(pass, user.mdp);
-    console.log(isMatch);
-    if (isMatch) {
-      return result;
-    }
+    const isMatch = (motDePasse == user.mdp);
+    // const isMatch = await bcrypt.compare(motDePasse == user.mdp);
+    if (isMatch) return result;
     return null;
   }
 
   async login(user: any) {
     const payload = { username: user.email, sub: user.id };
-    console.log(payload);
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    return { access_token: this.jwtService.sign(payload) };
   }
 }
