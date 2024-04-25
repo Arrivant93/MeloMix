1870564adeb178d31f7496d704f2d92e7dc21148
Tony95Montana <100350821+Tony95Montana@users.noreply.github.com>
2024-04-25 13:46:29 +0200

    Update musique.service.ts

--- src/musique/musique.service.ts
+++ src/musique/musique.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,21 +9,20 @@ export class MusiqueService {
     constructor(@InjectRepository(MusiqueEntity) private MusiquesRepository: Repository<MusiqueEntity>) { }
 
     async getMusiques(): Promise<MusiqueEntity[]> {
-        return await this.MusiquesRepository.find({ relations: ['Artiste','Style'] });
+        return await this.MusiquesRepository.find({ relations: ['Artiste', 'Style'] });
     }
-
-    async getMusique(_id: number): Promise<MusiqueEntity> {
-        return await this.MusiquesRepository.findOneBy({id:_id})
+    async getMusique(id: number): Promise<MusiqueEntity> {
+        return await this.MusiquesRepository.findOneBy({ id: id });
     }
-
+    async getMusiqueByName(titre: string): Promise<MusiqueEntity> {
+        return await this.MusiquesRepository.findOneBy({ titre: titre });
+    }
     async createMusique(Musique: MusiqueEntity) {
-        return await this.MusiquesRepository.save(Musique)
+        return await this.MusiquesRepository.save(Musique);
     }
-
     async updateMusique(Musique: MusiqueEntity) {
         return await this.MusiquesRepository.update(Musique.id, Musique);
     }
-
     async deleteMusique(Musique: MusiqueEntity) {
         return await this.MusiquesRepository.delete(Musique);
     }
