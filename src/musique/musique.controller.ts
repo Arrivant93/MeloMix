720f82ec12637e9534603ec12e220fbecc6c1a75
Tony95Montana <100350821+Tony95Montana@users.noreply.github.com>
2024-04-25 13:46:06 +0200

    Update musique.controller.ts

--- src/musique/musique.controller.ts
+++ src/musique/musique.controller.ts
@@ -1,32 +1,32 @@
-import { Controller, Post, Body, Get, Put, Delete,Param, UseGuards} from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Delete, Param } from '@nestjs/common';
 import { MusiqueService } from './musique.service';
 import { MusiqueEntity } from "src/musique/musique.entity/musique.entity";
 
 @Controller('Musique')
 export class MusiqueController {
+
     constructor(private service: MusiqueService) { }
 
     @Get()
     getAll() {
         return this.service.getMusiques();
     }
-
     @Get(':id')
     get(@Param() params) {
         return this.service.getMusique(params.id);
-
     }
-
+    @Get('/titre/:titre')
+    getByName(@Param() params) {
+        return this.service.getMusiqueByName(params.titre);
+    }
     @Post()
     create(@Body() Musique: MusiqueEntity) {
         return this.service.createMusique(Musique);
     }
-
     @Put()
     update(@Body() Musique: MusiqueEntity) {
         return this.service.updateMusique(Musique);
     }
-
     @Delete(':id')
     deleteUser(@Param() params) {
         return this.service.deleteMusique(params.id);
